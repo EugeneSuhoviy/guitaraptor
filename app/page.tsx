86ac2a46b64de54badcca6c92d4d7125449230be
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-18 16:50:39 +0300

    Changelog: - Added exercises container

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Exercise from "./components/exercise";
 import Link from 'next/link'
 import { getAllExercises } from "@/app/lib/actions";
+import ExercisesContainer from "./components/exercises-container";
 
 export default async function Home() {
   const exercises = await getAllExercises()
@@ -9,12 +10,7 @@ export default async function Home() {
   return (
     <>
       <main className="p-5">
-        {exercises?.map((item, index) => {
-          return <div key={index}>
-            <Exercise name={item.name} bpm={item.bpm} duration={item.duration} id={item.id} />
-          </div>
-        })}
-
+        <ExercisesContainer exercises={exercises} />
         <Link role="button" className="btn" href="/exercise/create">Add exercise</Link>
       </main>
     </>
