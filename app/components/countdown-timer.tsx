2afd2893b04734e73a68c36754c51ff7d3f43553
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-21 16:36:06 +0300

    Changelog: - Fixed types

--- app/components/countdown-timer.tsx
+++ app/components/countdown-timer.tsx
@@ -1,17 +1,22 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function CountdownTimer({ initialTime }: { initialTime: number }) {
     const time = initialTime * 60;
     const [timeRemaining, setTimeRemaining] = useState(time);
+    const ref = useRef<HTMLDialogElement>(null);
 
     useEffect(() => {
         const timerInterval = setInterval(() => {
             setTimeRemaining((prevTime) => {
                 if (prevTime === 0) {
                     clearInterval(timerInterval);
-                    alert('Countdown complete!');
+
+                    if (ref.current) {
+                        ref.current.showModal();
+                    }
+
                     return 0;
                 } else {
                     return prevTime - 1;
@@ -29,11 +34,24 @@ export default function CountdownTimer({ initialTime }: { initialTime: number })
     const seconds = timeRemaining % 60;
 
     return (
-        <div>
-            <span className="countdown font-mono text-6xl bg-accent p-1">
-                <span style={{ ['--value' as string]: minutes }}></span>:
-                <span style={{ ['--value' as string]: seconds }}></span>
-            </span>
-        </div>
+        <>
+            <div>
+                <span className="countdown font-mono text-6xl bg-accent p-1">
+                    <span style={{ ['--value' as string]: minutes }}></span>:
+                    <span style={{ ['--value' as string]: seconds }}></span>
+                </span>
+            </div>
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle" ref={ref}>
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Done!</h3>
+                    <p className="py-4">Press ESC key or click the button below to close</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </>
     );
 };
