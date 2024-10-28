2612404d3f5dc3f209789fc445cb126b6692a5fc
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-28 11:38:55 +0200

    Changelog: - Added auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import LoginContainer from "../components/login/login-container";
 
-export default function Login({ params }: { params: { id: string } }) {
+export default function Login() {
     return (
         <>
             <LoginContainer />
