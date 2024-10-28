cc582a7cd53815bf7d22dc232c05af3fc30e94c0
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-28 16:20:58 +0200

    Changelog: - Updated env

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,3 @@
 export const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 export const supabaseAnonkey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
