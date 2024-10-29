59028ca89126ea207ea25e01dbcfd0375366cfb5
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-29 12:59:01 +0200

    Changelog: - Add back btn

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,22 +1,30 @@
 'use client'
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { ChevronLeftIcon } from '@heroicons/react/24/outline';
 
 export default function Header() {
     const pathname = usePathname();
+    const router = useRouter();
+
+    let isEditPage = false;
 
     if (pathname.includes('login')) return
+
+    if (pathname.includes('edit') || pathname.includes('create')) {
+        isEditPage = true
+    }
 
     return <>
         <div className="p-5 flex items-center max-w-3xl mx-auto">
             <div>
-                <label className="btn btn-circle swap swap-rotate">
+                {isEditPage ? <label className="btn btn-circle"><ChevronLeftIcon className="size-8" onClick={() => { router.back() }} /></label> : <label className="btn btn-circle swap swap-rotate">
                     {/* this hidden checkbox controls the state */}
                     <input type="checkbox" />
 
                     {/* hamburger icon */}
-                    
+
                     <svg
                         className="swap-off fill-current"
                         xmlns="http://www.w3.org/2000/svg"
@@ -36,9 +44,9 @@ export default function Header() {
                         <polygon
                             points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
                     </svg>
-                </label>
+                </label>}
             </div>
-            <Link href={'/'} className="mx-auto"><h1 className="mx-auto font-bold text-3xl">GUITARAPTOR</h1></Link> 
+            <Link href={'/'} className="mx-auto"><h1 className="mx-auto font-bold text-3xl">GUITARAPTOR</h1></Link>
             <div>
                 <label className="swap swap-rotate">
                     {/* this hidden checkbox controls the state */}
