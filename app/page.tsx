7f2754aa4701af92e9bbbc9122b6c5cc60868c4c
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-30 19:29:53 +0200

    Changelog: - Added logout btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { getAllExercises } from "@/app/lib/actions";
 import ExercisesContainer from "./components/exercises-container";
-import LogoutBtn from './components/login/logout-btn';
 
 export default async function Home() {
   const exercises = await getAllExercises()
@@ -9,7 +8,6 @@ export default async function Home() {
     <>
       <main className="p-5">
         <ExercisesContainer exercises={exercises} />
-        <LogoutBtn />
       </main>
     </>
   );
