b747380f41fc32b2dfdc30234a9777b44bab3ed5
EugeneSuhoviy <suhoviy.eugene@gmail.com>
2024-10-30 13:33:21 +0200

    Changelog: - Updated skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,53 +1,21 @@
 export default function Loading() {
-  return <main className="p-3 animate-pulse max-w-3xl mx-auto">
-    <div>
-      <div className="mb-1 w-full flex items-center p-5">
-        <div className="h-10 w-20 bg-gray-200 rounded mr-5"></div>
-        <div className="flex items-left flex-col mr-auto">
-          <div className="h-4 w-24 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
+  const SKELETON_LINE_NUMBER = 4;
+
+  return <main className="p-3 max-w-3xl mx-auto">
+    {[...Array(SKELETON_LINE_NUMBER)].map((_, index) => (
+      <div key={index} className="flex w-full flex-col gap-4 mb-4">
+        <div className="flex items-center gap-4 p-5">
+          <div className="skeleton h-10 w-20 shrink-0 rounded mr-5"></div>
+          <div className="flex flex-col gap-2 mr-auto">
+            <div className="skeleton h-4 w-24"></div>
+            <div className="skeleton h-4 w-16"></div>
+            <div className="skeleton h-4 w-16"></div>
+          </div>
+          <div className="skeleton h-10 w-20 rounded"></div>
         </div>
-        <div className="h-10 w-20 bg-gray-200 rounded"></div>
+        <div className="skeleton h-1 w-full rounded"></div>
       </div>
-      <div className="divider h-1 bg-gray-200 rounded w-full"></div>
-    </div>
-    <div>
-      <div className="mb-1 w-full flex items-center p-5">
-        <div className="h-10 w-20 bg-gray-200 rounded mr-5"></div>
-        <div className="flex items-left flex-col mr-auto">
-          <div className="h-4 w-24 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-        </div>
-        <div className="h-10 w-20 bg-gray-200 rounded"></div>
-      </div>
-      <div className="divider h-1 bg-gray-200 rounded w-full"></div>
-    </div>
-    <div>
-      <div className="mb-1 w-full flex items-center p-5">
-        <div className="h-10 w-20 bg-gray-200 rounded mr-5"></div>
-        <div className="flex items-left flex-col mr-auto">
-          <div className="h-4 w-24 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-        </div>
-        <div className="h-10 w-20 bg-gray-200 rounded"></div>
-      </div>
-      <div className="divider h-1 bg-gray-200 rounded w-full"></div>
-    </div>
-    <div>
-      <div className="mb-1 w-full flex items-center p-5">
-        <div className="h-10 w-20 bg-gray-200 rounded mr-5"></div>
-        <div className="flex items-left flex-col mr-auto">
-          <div className="h-4 w-24 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-          <div className="h-4 w-16 bg-gray-200 rounded mb-1"></div>
-        </div>
-        <div className="h-10 w-20 bg-gray-200 rounded"></div>
-      </div>
-      <div className="divider h-1 bg-gray-200 rounded w-full"></div>
-    </div>
-    <div className="h-10 w-32 bg-gray-200 rounded mt-5"></div>
-  </main>;
+    ))}
+    <div className="skeleton h-10 w-32 rounded mt-5"></div>
+  </main>
 }
